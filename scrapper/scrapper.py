5629d0b3c342b203383bc1523ea2080f0454073e
Rohan Karan <rohankaran001@gmail.com>
2022-03-13 17:17:14 +0530

    initial commit

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -12,7 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-sh-usage")
+chrome_options.add_argument("--disable-dev-shm-usage")
 month_map = {
     'jan': 1,
     'feb': 2,
