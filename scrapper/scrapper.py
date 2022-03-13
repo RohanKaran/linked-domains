7d6199ec3e256d7d47bf01d13b07568edae75cba
Rohan Karan <rohankaran001@gmail.com>
2022-03-13 19:13:26 +0530

    initial commit

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -26,8 +26,8 @@ month_map = {
 
 chrome_options = Options()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--disable-notifications")
@@ -63,7 +63,6 @@ class Scraper(webdriver.Chrome):
         print("logged in")
 
     def scrape(self, target, last_domain, last_date):
-        self.login('rahulthepcl@gmail.com', 'Adsense007##')
         dr = 1
         all_links = []
 
